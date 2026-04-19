6005bac9fb375f643c77a4fe7ece264e4c812aa5
Gajjala Trishna <147131793+trishna2105@users.noreply.github.com>
2026-04-19 16:15:30 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:Nord9182finance@db.oyhpuoebrydygxrffidy.supabase.co:5432/postgres"
+DATABASE_URL = "URL HERE"
 
 
 
